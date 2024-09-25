9f6f6bcf44540d6607f78c995ed37ddd4a308f74
Francisco Sanchez <francisco.sanchez@kauky.com>
2024-09-25 11:42:12 +0200

    yesterday aplied

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.deepOrange, Colors.black],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset('assets/images/quiz-logo.png', width: 400),
+                const Text(
+                  'Learn Fluetter the fun way!',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  child: const Text("star quiz"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
